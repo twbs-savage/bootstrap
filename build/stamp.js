13a1dbd3698ae69ca33380d07d9b6124353dd509
Bardi Harborow <bardi@bardiharborow.com>
2017-03-12 18:39:00 +1100

    Build system overhaul.

---
+++ build/stamp.js
@@ -0,0 +1,16 @@
+/*!
+ * Bootstrap <%= pkg.version %> (<%= pkg.homepage %>)
+ * Copyright 2011-<%= grunt.template.today("yyyy") %> The Bootstrap Authors
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+
+if (typeof jQuery === 'undefined') {
+  throw new Error('Bootstrap\'s JavaScript requires jQuery. jQuery must be included before Bootstrap\'s JavaScript.')
+}
+
+(function ($) {
+  var version = $.fn.jquery.split(' ')[0].split('.')
+  if ((version[0] < 2 && version[1] < 9) || (version[0] == 1 && version[1] == 9 && version[2] < 1) || (version[0] >= 4)) {
+    throw new Error('Bootstrap\'s JavaScript requires at least jQuery v1.9.1 but less than v4.0.0')
+  }
+})(jQuery);

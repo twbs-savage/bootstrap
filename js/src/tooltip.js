20bb5670c23268e16e693d7380862d214664efa2
Johann-S <johann.servoire@gmail.com>
2017-04-19 15:08:06 +0200

    Add an update method to allow to update position for Tooltip/Popover/Dropdown manually

--- js/src/tooltip.js
+++ js/src/tooltip.js
@@ -227,6 +227,9 @@ const Tooltip = (($) => {
       this._timeout       = null
       this._hoverState    = null
       this._activeTrigger = null
+      if (this._popper !== null) {
+        this._popper.destroy()
+      }
       this._popper        = null
 
       this.element = null
@@ -386,6 +389,11 @@ const Tooltip = (($) => {
 
     }
 
+    update() {
+      if (this._popper !== null) {
+        this._popper.scheduleUpdate()
+      }
+    }
 
     // protected
 
@@ -647,8 +655,8 @@ const Tooltip = (($) => {
     }
 
     _handlePopperPlacementChange(data) {
-        this._cleanTipClass()
-        this.addAttachmentClass(this._getAttachment(data.placement))
+      this._cleanTipClass()
+      this.addAttachmentClass(this._getAttachment(data.placement))
     }
 
     // static
